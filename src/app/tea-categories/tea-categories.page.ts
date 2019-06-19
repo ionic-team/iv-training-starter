5efdf4c5e63b96502f280d0e2f9e7d65b42e2eb4
Ken Sodemann <ken@ionic.io>
2019-06-19 15:28:36 -0500

    feat(auth): complete the login workflow

--- src/app/tea-categories/tea-categories.page.ts
+++ src/app/tea-categories/tea-categories.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+
+import { AuthenticationService } from '../services/authentication/authentication.service';
+import { TeaCategory } from '../models/tea-category';
+import { TeaCategoriesService } from '../services/tea-categories/tea-categories.service';
 
 @Component({
   selector: 'app-tea-categories',
   templateUrl: 'tea-categories.page.html',
-  styleUrls: ['tea-categories.page.scss'],
+  styleUrls: ['tea-categories.page.scss']
 })
-export class TeaCategoriesPage {
+export class TeaCategoriesPage implements OnDestroy, OnInit {
+  private subscription: Subscription;
+  categories: Array<TeaCategory>;
 
-  constructor() {}
+  constructor(
+    private authentication: AuthenticationService,
+    private navController: NavController,
+    private teaCategories: TeaCategoriesService
+  ) {}
 
+  ngOnInit() {
+    this.subscription = this.teaCategories.changed.subscribe(() => this.getData());
+  }
+
+  ionViewDidEnter() {
+    this.getData();
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  logout() {
+    this.authentication.logout().subscribe(() => this.navController.navigateRoot('/login'));
+  }
+
+  editCategory(id: number) {
+    this.navController.navigateForward(['edit-tea-category', id]);
+  }
+
+  private getData() {
+    this.teaCategories.getAll().subscribe(x => (this.categories = x));
+  }
 }
