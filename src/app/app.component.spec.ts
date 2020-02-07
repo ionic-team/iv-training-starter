ce9a47fa5e689d4e96e2042c037d26c3580abfad
Ken Sodemann <ken@ionic.io>
2020-02-07 05:10:49 -0600

    chore(app): upgrade dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Plugins, StatusBarStyle } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { createPlatformMock } from 'test/mocks';
 
 describe('AppComponent', () => {
   let originalSplashScreen;
@@ -15,6 +17,7 @@ describe('AppComponent', () => {
     Plugins.SplashScreen = jasmine.createSpyObj('SplashScreen', ['hide']);
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [{ provide: Platform, useFactory: createPlatformMock }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
@@ -31,19 +34,40 @@ describe('AppComponent', () => {
   });
 
   describe('initialization', () => {
-    it('hides the splash screen', fakeAsync(() => {
-      TestBed.createComponent(AppComponent);
-      tick();
-      expect(Plugins.SplashScreen.hide).toHaveBeenCalledTimes(1);
-    }));
-
-    it('sets the status bar style to light', fakeAsync(() => {
-      TestBed.createComponent(AppComponent);
-      tick();
-      expect(Plugins.StatusBar.setStyle).toHaveBeenCalledTimes(1);
-      expect(Plugins.StatusBar.setStyle).toHaveBeenCalledWith({
-        style: StatusBarStyle.Light
+    describe('as a hybrid mobile app', () => {
+      beforeEach(() => {
+        const platform = TestBed.get(Platform);
+        platform.is.withArgs('hybrid').and.returnValue(true);
       });
-    }));
+
+      it('hides the splash screen', fakeAsync(() => {
+        TestBed.createComponent(AppComponent);
+        tick();
+        expect(Plugins.SplashScreen.hide).toHaveBeenCalledTimes(1);
+      }));
+
+      it('sets the status bar style to light', fakeAsync(() => {
+        TestBed.createComponent(AppComponent);
+        tick();
+        expect(Plugins.StatusBar.setStyle).toHaveBeenCalledTimes(1);
+        expect(Plugins.StatusBar.setStyle).toHaveBeenCalledWith({
+          style: StatusBarStyle.Light
+        });
+      }));
+    });
+
+    describe('as any app other than hybrid mobile', () => {
+      it('does not hide the splash screen', fakeAsync(() => {
+        TestBed.createComponent(AppComponent);
+        tick();
+        expect(Plugins.SplashScreen.hide).not.toHaveBeenCalled();
+      }));
+
+      it('does not set the status bar style', fakeAsync(() => {
+        TestBed.createComponent(AppComponent);
+        tick();
+        expect(Plugins.StatusBar.setStyle).not.toHaveBeenCalled();
+      }));
+    });
   });
 });
